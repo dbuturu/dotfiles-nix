7ec9dfe4df3e97672d1b0f3d4e7c2ccf19060a86
Daniel Buturu <dbuturu@gmail.com>
2025-07-17 12:16:08 +0300

    Alphabetize packages in modules/packages

--- modules/packages/default.nix
+++ modules/packages/default.nix
@@ -11,36 +11,36 @@ in {
     options.modules.packages = { enable = mkEnableOption "packages"; };
     config = mkIf cfg.enable {
     	home.packages = with pkgs; [
-            ripgrep tealdeer
-            gnupg
-            grim slurp slop
-            imagemagick age
-            python3 lua  pqiv
-            screen bandw maintenance
-            wf-recorder anki-bin #arkenfox-user.js #exfat-utils
-            eww #gtk-theme-arc-gruvbox-git
-            #hyprpaper #libertinus-font 
-            #maim #"can take quick screenshots at your request."
-            #nsixv #ntfs-3g #task-spooler
-            #tesseract-data-eng #ttf-dejavu #ttf-font-awesome
-            #zathura-pdf-mupdf #zsh-fast-syntax-highlighting-git abook
-            ani-cli arandr atool android-tools
-            bat bc bluetui btop
+            abook age ani-cli anki-bin aria2 arandr atool android-tools
+            #arkenfox-user.js 
+            bat bandw bc bluetui btop
             calcurse corefonts clipboard-jh cups
-            dosfstools dunst eza ffmpeg
-            ffmpegthumbnailer firefox fzf 
-            git git-lfs gitAndTools.delta gitAndTools.gh gitAndTools.git-fame
-            gnome-keyring greetd.regreet hyprland kitty
-            lf libnotify libreoffice lowdown
-            lynx mediainfo mindustry moreutils mutt-wizard
-            mpc mpd mpv
-            ncmpcpp neomutt
-            networkmanagerapplet noto-fonts noto-fonts-emoji
-            onlyoffice-desktopeditors pass poppler pulsemixer qpwgraph sc-im
-            simple-mtpfs slock socat
-            swww tesseract unzip
-            vscode winbox4 wireplumber wofi
-            yt-dlp zathura zig zk
+            dosfstools dunst
+            eza
+            #exfat-utils
+            ffmpeg ffmpegthumbnailer firefox fzf 
+            git git-lfs gitAndTools.delta gitAndTools.gh gitAndTools.git-fame gnome-keyring gnupg greetd.regreet grim 
+            hyprland
+            #hyprpaper
+            imagemagick
+            lf libnotify libreoffice lowdown lua lynx
+            #libertinus-font 
+            maintenance mediainfo mindustry moreutils mutt-wizard mpc mpd mpv 
+            #maim #"can take quick screenshots at your request."
+            ncmpcpp nerd-fonts.fira-code neomutt networkmanagerapplet noto-fonts noto-fonts-emoji
+            #nsixv #ntfs-3g
+            pass poppler pqiv python3 pulsemixer
+            qpwgraph
+            ripgrep
+            screen sc-im simple-mtpfs slock slop socat slurp swww
+            tealdeer tesseract
+            #task-spooler #tesseract-data-eng #ttf-dejavu #ttf-font-awesome
+            unzip
+            vscode
+            wf-recorder winbox4 wireplumber wofi
+            yt-dlp
+            zathura zig zip zk
+            #zathura-pdf-mupdf #zsh-fast-syntax-highlighting-git 
         ];
     };
 }
